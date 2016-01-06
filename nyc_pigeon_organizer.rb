07f45d40a89f0243f99750bb5b57b14dd332727e
dougtebay <dougtebay@gmail.com>
2016-01-06 08:23:56 -0500

    Done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |category, pigeon_data|
+    pigeon_data.each do |attribute, pigeons|
+      pigeons.each do |pigeon|
+        new_hash[pigeon] = {color: [], gender: [], lives: []} unless new_hash[pigeon]
+        new_hash[pigeon][:color] << attribute.to_s if category == :color && pigeons.include?(pigeon)
+        new_hash[pigeon][:gender] << attribute.to_s if category == :gender && pigeons.include?(pigeon)
+        new_hash[pigeon][:lives] << attribute if category == :lives && pigeons.include?(pigeon)
+      end
+    end
+  end
+  new_hash
 end
